28a132a58534530b5014242ce611199652791e1b
itsara <itsara@kha-macbookpro-M2.local>
2023-06-14 22:07:14 +0700

    update logic generate block and check link

--- scenes/Block.gd
+++ scenes/Block.gd
@@ -1,17 +1,36 @@
 extends AnimatedSprite
 
-
-var type = 0
+var blocktype = 0
 var connect_left = false
 var connect_right = false
 var connect_top = false
 var connect_bottom = false
 
-# Called when the node enters the scene tree for the first time.
+var links = []
+
 func _ready():
-	pass # Replace with function body.
+	_set_connect(self.rotation_degrees)
+		
+	
+func _set_connect(degree):	
+	if self.blocktype == 0:		
+		match (int(degree)):
+			0, 180:
+				connect_left = true
+				connect_right = true
+				connect_top = false
+				connect_bottom = false
+			90, 270:
+				connect_left = false
+				connect_right = false
+				connect_top = true
+				connect_bottom = true			
+			_:
+				pass
+		
 
+func clear_link_block():
+	for link_block in links:
+		link_block.links.remove(self)
+	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
--- scenes/Play.gd
+++ scenes/Play.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 
+const BLOCK_W = 80
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body
@@ -14,4 +16,85 @@ func _ready():
 
 func create_block():
 	print("create_block")
-	pass
+	var data = []		
+	for r in range (0, 5):
+		var row = []
+		for c in range(0, 4):			
+			var block = gen_block( r, c)					
+			self.add_child(block)			
+			
+			#check left chain
+			if c != 0:
+				if block.connect_left == true and row[c-1].connect_right == true:
+					block.links.append(row[c-1])
+					row[c-1].links.append(block)
+			
+			#check top chain
+			if r != 0:	
+				if block.connect_top == true and data[r-1][c].connect_bottom == true:
+					block.links.append(data[r-1][c])
+					data[r-1][c].links.append(block)							
+			row.append(block)			
+		data.append(row)			
+	
+	show_data(data)
+	show_connect(data)
+	show_block_detail(data)
+	
+	
+func show_data(data):
+	for r in range(0, 5):
+		var s = ""
+		for c in range (0, 4):
+			s += str(data[r][c].rotation_degrees) + ", "
+				
+		print(s)
+		
+func show_connect(data):
+	print("--")
+	for r in range(0, 5):
+		var s = ""
+		for c in range (0, 4):
+			s += str(len(data[r][c].links)) + ", "				
+		print(s)
+
+func show_block_detail(data):
+	print("--")
+	for r in range(0, 5):		
+		for c in range (0, 4):
+			print(r, " ", c, ": ", 
+				data[r][c].connect_left, " ", 
+				data[r][c].connect_right," ", 
+				data[r][c].connect_top, " ", 
+				data[r][c].connect_bottom)
+			
+
+
+func gen_block(r, c):	
+	
+	var block = preload("res://scenes/Block.tscn").instance()	
+	block.position.x = c * BLOCK_W + (BLOCK_W/2)
+	block.position.y = r * BLOCK_W + (BLOCK_W/2)	
+	block.blocktype = 0
+	block.rotation_degrees = 0
+	
+#	var rng = RandomNumberGenerator.new()
+#	rng.randomize()
+#	var random = rng.randi_range(0, 3)
+#	var degree = 90 * random
+#
+#	block.rotation_degrees = degree
+	
+	
+	
+	# rotate_block(block, degree)
+	return block
+	
+func rotate_block(block, degree):
+	var tween = Tween.new()
+	add_child(tween)
+	var rotVal = degree * (PI / 180)
+	var delay = 1
+	tween.interpolate_property(block, "rotation", self.rotation, self.rotation+rotVal, delay, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
+	
